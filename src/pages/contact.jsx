035309eca0f355765695c38603c3decd7157a038
Alex Abreu <alex.abreu@sendgrid.com>
2019-05-31 13:45:48 -0600

    contact numbers

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -9,12 +9,12 @@ export default () => (
   >
     <h4>When is the Downeast Treatment Center open?</h4>
 
-    <p>We are open Monday and Thursday from 8AM to 8PM. We hold group counseling sessions in the morning, at midday, and in the evening in order to provide the greatest access to those who are are in need of treatment.</p>
+    <p>We are open Monday and Thursday from 8 AM to 8 PM. We hold group counseling sessions in the morning, afternoon from 1:30 PM to 3:15 PM, and in the evening from 5:30 PM to 7:15 PM in order to provide the greatest access to those who are in need of treatment.</p>
 
 
     <h4>How do I make an appointment?</h4>
 
-    <p>To schedule an appointment, call <a className="primary" href="tel:(800) 244-6431">(800) 244-6431</a></p>
+    <p>To schedule an appointment during business hours, call <a className="primary" href="tel:(207) 667-6890">(207) 667-6890</a>. To speak with someone when the office is closed, call <a className="primary" href="tel:(800) 244-6431">(800) 244-6431</a>.</p>
 
 
     <h4>Where is the Downeast Treatment Center located?</h4>
