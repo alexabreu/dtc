ee3eb12ea7a5726e9f2d4f8139b9898a4668cb52
Alex Abreu <alexabreu@gmail.com>
2019-07-21 21:05:04 -0400

    more margin around index page blurb

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,7 +13,7 @@ export default () => (
       <p>Our program is now more client-centered and flexible than ever. Our team will work with you to create a treatment plan that works for you and your schedule. With evening hours and customized treatment, we can help you succeed in recovery.</p>
       <p>Free services available. Customized approach. Confidential. Respectful. Compassionate.</p>
       <img src="/images/building.jpg" alt="DownEast Treatment Center Facility" title="DownEast Treatment Center Facility" />
-      <p>The DownEast Treatment Center is the hub of a "hub and spoke" treatment model for substance use conditions developed by the DownEast Treatment Network partnership. We offer comprehensive plans, including medication and counseling, for treatment of substance use conditions, including opioid use disorder. We offer help and hope to people struggling with chemical dependency, addiction, or substance misuse.</p>
+      <p style={{margin: '3em 0 4em'}}>The DownEast Treatment Center is the hub of a "hub and spoke" treatment model for substance use conditions developed by the DownEast Treatment Network partnership. We offer comprehensive plans, including medication and counseling, for treatment of substance use conditions, including opioid use disorder. We offer help and hope to people struggling with chemical dependency, addiction, or substance misuse.</p>
       <iframe title="DownEast Treatment Center Google Map" width="100%" height="450" frameBorder="0" style={{border: 0}} src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJZ2MmofvwrkwR_QdhMz1Cxvc&key=AIzaSyA7-3BW_JUzZrZ6-HK-0IQBY2_bRlHeCDM" allowFullScreen></iframe>
   </Main>
 )
