bb75413ad20819267686bc323a35a5b1a43980a8
Alex Abreu <alexabreu@gmail.com>
2019-07-28 13:37:04 -0400

    logo titles

--- src/components/partners.jsx
+++ src/components/partners.jsx
@@ -8,17 +8,17 @@ export default () => (
     <h2>Our Partnerships</h2>
     <p>The DownEast Treatment Center is made possible by numerous community partners, the Health Resources and Services Administration, Maine's Substance Abuse and Mental Health Services, U.S. Substance Abuse and Mental Health Administration, Maine Health Access Foundation, and the Stephen &amp; Tabitha King Foundation, along with key organizational support from the following groups.</p>
     <div className={Styles.logos}>
-      <div className={cn(Styles.logo, Styles.ha)} />
-      <div className={cn(Styles.logo, Styles.amhc)} />
-      <div className={cn(Styles.logo, Styles.brhc)} />
-      <div className={cn(Styles.logo, Styles.hrsa)} />
-      <div className={cn(Styles.logo, Styles.mdih)} />
-      <div className={cn(Styles.logo, Styles.mehaf)} />
-      <div className={cn(Styles.logo, Styles.nlbhh)} />
-      <div className={cn(Styles.logo, Styles.nlmch)} />
-      <div className={cn(Styles.logo, Styles.samhs)} />
-      <div className={cn(Styles.logo, Styles.stkf)} />
-      <div className={cn(Styles.logo, Styles.samhsa)} />
+      <div className={cn(Styles.logo, Styles.ha)} title="Healthy Acadia Logo"/>
+      <div className={cn(Styles.logo, Styles.amhc)} title="AMHC Logo"/>
+      <div className={cn(Styles.logo, Styles.brhc)} title="Bucksport Regional Health Center Logo"/>
+      <div className={cn(Styles.logo, Styles.hrsa)} title="Health Resources &amp; Services Administration Logo"/>
+      <div className={cn(Styles.logo, Styles.mdih)} title="Mount Desert Island Hospital Logo"/>
+      <div className={cn(Styles.logo, Styles.mehaf)} title="Maine Health Access Foundation Logo"/>
+      <div className={cn(Styles.logo, Styles.nlbhh)} title="Northern Light Blue Hill Hospital Logo"/>
+      <div className={cn(Styles.logo, Styles.nlmch)} title="Northern Light Maine Coast Hospital Logo"/>
+      <div className={cn(Styles.logo, Styles.samhs)} title="State of Maine Substance Abuse &amp; Mental Health Services Logo"/>
+      <div className={cn(Styles.logo, Styles.stkf)} title="STK Foundatation Logo"/>
+      <div className={cn(Styles.logo, Styles.samhsa)} title="Substance Abuse &amp; Mental Health Services Administration Logo"/>
     </div>
   </section>
 );
